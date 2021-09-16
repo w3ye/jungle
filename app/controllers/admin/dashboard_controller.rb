92ca4747ec36d058044cb94324a71ee1670c9312
William Ye <w3ye@pm.me>
2021-09-15 23:10:15 -0400

    merge feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
