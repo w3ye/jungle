18480e6050f8f61faab40b6a6529d9a206bd70d8
William Ye <w3ye@pm.me>
2021-09-17 01:15:11 -0400

    implement checks for status

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ActiveRecord::Base
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    sale.start_on > Date.current
+  end
+
+  def active? 
+    !upcoming? && !finished?
+  end
 end
