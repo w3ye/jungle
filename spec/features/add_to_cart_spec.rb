a5a907778ad2a6b35f1025a0b99f83f1e79b0485
William <w3ye@pm.me>
2021-09-22 17:15:50 -0400

    merge fix/add-to-cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -5,15 +5,13 @@ RSpec.feature "AddToCarts", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
-      @category.products.create!(
-        name: Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
+    @category.products.create!(
+      name: Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
   end
 
   scenario "The My Cart will increase by 1 if I add something to cart" do
